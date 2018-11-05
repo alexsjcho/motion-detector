f0ff6c62d5d554125372e880cc3364d4114a1385
Alex Cho <alexsjcho@gmail.com>
2018-11-04 17:46:35 -0800

    finished motion detector app

--- motion-detector/md-app.py
+++ motion-detector/md-app.py
@@ -1,44 +1,70 @@
-import cv2, time
+#Libraries and classes used
+import cv2, time, pandas
+from datetime import datetime
 
+#Objects
 first_frame=None
-
+status_list=[None,None]
+times=[]
+df=pandas.DataFrame(columns=["Start","End"])
 video=cv2.VideoCapture(0)
 
+#Capturing first frame rendering (Color Frames)
 while True:
     check, frame = video.read()
-
+    status=0
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     gray=cv2.GaussianBlur(gray,(21,21),0)
 
     if first_frame is None:
         first_frame=gray
         continue
-
+#Delta, Grayscale frame rendering
     delta_frame=cv2.absdiff(first_frame,gray)
     thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
 
-
+#Image contour rendering
     (_,cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-    for countour in cnts:
-        if cv2.countourArea(contour) < 1000:
+    for contour in cnts:
+        if cv2.contourArea(contour) < 10000:
             continue
-        (x,y,w,h)=cv2.boundRect(contour)
-        cv2.rectangle(frame, (x,y),(x+w, y+h),(0,255,0),3)
+        status=1
 
+        (x,y,w,h)=cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y),(x+w, y+h),(0,255,0),3)
+    status_list.append(status)
+
+#Time stamp rendering
+    if status_list[-1]==1 and status_list[-2]==0:
+        times.append(datetime.now())
+    if status_list[-1]==0 and status_list[-2]==1:
+        times.append(datetime.now())
+
+#Image rendering
 
     cv2.imshow("Gray Frame",gray)
     cv2.imshow("Delta Frame",delta_frame)
     cv2.imshow("Threshold Frame", thresh_frame)
     cv2.imshow("Color Frame", frame)
 
-
     key=cv2.waitKey(1)
-    print(gray)
 
     if key==ord('q'):
+        if status==1:
+            times.append(datetime.now())
         break
 
-    video.release()
-    cv2.destroyAllWindows
+#Call scripts
+print(status_list)
+print(times)
+
+#Recording DataFrame into CSV file
+for i in range(0,len(times),2):
+    df=df.append({"Start":times[i],"End":times[i+1]},ignore_index=True)
+
+df.to_csv("Times.csv")
+
+video.release()
+cv2.destroyAllWindows
