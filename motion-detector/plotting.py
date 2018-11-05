609becf170b42f5ea0656ae9a135265fbf7211c9
Alex Cho <alexsjcho@gmail.com>
2018-11-04 19:08:01 -0800

    finished app update

--- motion-detector/plotting.py
+++ motion-detector/plotting.py
@@ -1,17 +1,19 @@
+#Import Libraries and Classes
 from motion_detector import df
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import HoverTool, ColumnDataSource
 
+#Data plotting variables
 df["Start_string"]=df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
 df["End_string"]=df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
-
-
 cds=ColumnDataSource(df)
 
+#Data graph
 p=figure(x_axis_type='datetime',height=100, width=500, sizing_mode = "scale_width", title="Motion Graph")
 p.yaxis.minor_tick_line_color=None
 p.ygrid[0].ticker.desired_num_ticks=1
 
+#Hover pop-up information tool 
 hover=HoverTool(tooltips=[("Start","@Start_string"),("End","@End_string")])
 p.add_tools(hover)
 
